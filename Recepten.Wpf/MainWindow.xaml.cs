29b782438d6a5f2a4ba0907207ae8adb2e4717ee
Stef Spermalie <stef.vercaemer@spermalie.be>
2020-04-29 17:43:09 +0200

    Add project files.

---
+++ Recepten.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Recepten.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string pad = @"../../Assets/Artikelen2.csv";
+        const char DecimaalTeken = '.';
+
+        string[] verpakkingen =  { "100 g", "100 ml", "200 cl", "50 cl", "500 g", "75 cl", "Bot", "Doos",
+                                        "doosje", "Fles", "Kg", "Liter", "pak", "Portie", "Stuk", "zak" };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+         private void MaakGuiLeeg()
+        {
+            lstArtikelen.SelectedItem = null;
+            ClearPanel(grdArtikel);
+            cmbEenheid.SelectedIndex = 0;
+            txtPrijs.Text = "0";
+            txtArtikelnaam.Focus();
+            tbkFeedback.Visibility = Visibility.Hidden;
+        }
+
+
+    }
+}
---
+++ Recepten.Wpf/Utilities.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Recepten.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        void ToonMelding(string melding, bool isSucces = false)
+        {
+            tbkFeedback.Visibility = Visibility.Visible;
+            tbkFeedback.Text = melding;
+            tbkFeedback.Background = isSucces == true ? Brushes.DarkGreen : Brushes.Red;
+        }
+
+        bool IsGeldigGetal(TextBox textBox, Type type)
+        {
+            string getal;
+            string typeNaam = "";
+            bool inputCorrect = false;
+
+            getal = textBox.Text;
+
+            if (string.IsNullOrEmpty(getal.Trim()))
+            {
+                textBox.Focus();
+                textBox.SelectAll();
+                throw new Exception("Vul een getal in");
+            }
+            try
+            {
+                switch (type.Name)
+                {
+                    case "Int32":
+                        typeNaam = "geheel getal";
+                        int.Parse(getal);
+                        break;
+                    case "Single":
+                        typeNaam = "kommagetal";
+                        float.Parse(getal);
+                        break;
+                    case "Decimal":
+                        typeNaam = "decimaal getal";
+                        decimal.Parse(getal);
+                        break;
+                    default:
+                        break;
+                }
+                inputCorrect = true;
+            }
+            catch (Exception ex)
+            {
+                textBox.Focus();
+                textBox.SelectAll();
+                throw new Exception($"{getal} is geen geldig {typeNaam}\n" + ex.Message);
+            }
+            finally
+            {
+                MarkeerTextBox(textBox, inputCorrect);
+            }
+            return inputCorrect;
+        }
+
+        void MarkeerTextBox(TextBox textBox, bool inputCorrect)
+        {
+            if (inputCorrect)
+            {
+                textBox.BorderThickness = new Thickness(1);
+                textBox.BorderBrush = new SolidColorBrush(Colors.Gray);
+                textBox.Background = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                textBox.BorderThickness = new Thickness(3);
+                textBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                textBox.Background = new SolidColorBrush(Colors.LightPink);
+            }
+        }
+
+        private void IntegerTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInput((TextBox)sender, typeof(int));
+        }
+
+        private void FloatTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInput((TextBox)sender, typeof(float));
+        }
+
+        private void DecimalTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInput((TextBox)sender, typeof(decimal));
+        }
+
+        void CheckInput(TextBox textBox, Type type)
+        {
+            try
+            {
+                if (IsGeldigGetal(textBox, type))
+                {
+                    tbkFeedback.Visibility = Visibility.Hidden;
+                }
+            }
+            catch (Exception ex)
+            {
+                ToonMelding(ex.Message);
+            }
+        }
+
+        void ClearPanel(Panel toClear)
+        {
+            foreach (object control in toClear.Children)
+            {
+                Console.WriteLine(control.ToString());
+                if (control is TextBox)
+                {
+                    TextBox castedControl = (TextBox)control;
+                    castedControl.Text = string.Empty;
+                }
+                else if (control is Label)
+                {
+                    Label castedControl = (Label)control;
+                    if (castedControl.Name.Length > 0)
+                    {
+                        castedControl.Content = "";
+                    }
+                }
+                else if (control is TextBlock)
+                {
+                    TextBlock castedControl = (TextBlock)control;
+                    if (castedControl.Name.Length > 0)
+                    {
+                        castedControl.Text = "";
+                    }
+                }
+                else if (control is Selector)
+                {
+                    Selector castedControl = (Selector)control;
+                    castedControl.SelectedIndex = -1;
+                }
+                else if (control is DatePicker)
+                {
+                    DatePicker castedControl = (DatePicker)control;
+                    castedControl.SelectedDate = DateTime.Today;
+                }
+                else if (control is Slider)
+                {
+                    Slider castedControl = (Slider)control;
+                    castedControl.Value = castedControl.Minimum;
+                }
+                else if (control is CheckBox)
+                {
+                    CheckBox castedControl = (CheckBox)control;
+                    castedControl.IsChecked = false;
+                }
+            }
+
+        }
+
+    }
+}
